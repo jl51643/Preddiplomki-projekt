aa57c584162e41e5e610483bfa7a6acb30fdec81
Benjamin Horvat <bh51358@fer.hr>
2020-12-04 16:23:14 +0100

    Stvoren backend spring boot projekt te dodane konfiguracije.

---
+++ Software/backend/SmartAgriculture/src/test/java/hr/fer/tel/SmartAgriculture/SmartAgricultureApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.tel.SmartAgriculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAgricultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
