9ebf0a428f0db03671c366377a524e69b4edb341
Benjamin Horvat <bh51358@fer.hr>
2021-01-11 15:16:54 +0100

    Dodano slanje login tokena u jsonu

--- Software/backend/SmartAgriculture/src/main/java/hr/fer/tel/SmartAgriculture/controllers/UserController.java
+++ Software/backend/SmartAgriculture/src/main/java/hr/fer/tel/SmartAgriculture/controllers/UserController.java
@@ -2,11 +2,10 @@ package hr.fer.tel.SmartAgriculture.controllers;
 
 import hr.fer.tel.SmartAgriculture.models.LoginModel;
 import hr.fer.tel.SmartAgriculture.models.RegistrationModel;
+import hr.fer.tel.SmartAgriculture.models.TokenModel;
 import hr.fer.tel.SmartAgriculture.services.UserService;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,13 +25,23 @@ public class UserController {
     public ResponseEntity<?> login(@RequestBody LoginModel loginRequest) {
         String jwt = userService.login(loginRequest);
 
-        return ResponseEntity.ok(jwt);
+        return ResponseEntity.ok(new TokenModel(jwt, loginRequest.getUsername()));
     }
 
     @PostMapping("/auth/register")
     public ResponseEntity<?> register(@RequestBody RegistrationModel registrationModel) {
-        String jwt = userService.register(registrationModel);
-
-        return ResponseEntity.ok(jwt);
+        try {
+            String jwt = userService.register(registrationModel);
+            return ResponseEntity.ok(new TokenModel(jwt, registrationModel.getUsername()));
+        }
+        catch (DataIntegrityViolationException e) {
+            String message = e.getMostSpecificCause().getMessage();
+            if (message.contains("Users_username_key")) {
+                return ResponseEntity.badRequest().body("Non unique username.");
+            } else if (message.contains("Users_email_key")) {
+                return ResponseEntity.badRequest().body("Non unique email");
+            }
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
